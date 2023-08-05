080a190a098e837cfefacb595a3762e469c21af3
Bullets-jyr <jyr@bullets.co.kr>
2023-08-05 12:08:21 +0900

    18. Create HeroRepository

---
+++ src/main/kotlin/kr/co/bullets/repository/HeroRepository.kt
@@ -0,0 +1,18 @@
+package kr.co.bullets.repository
+
+import kr.co.bullets.models.ApiResponse
+import kr.co.bullets.models.Hero
+
+interface HeroRepository {
+
+    val heroes: Map<Int, List<Hero>>
+
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+    suspend fun searchHeroes(name: String?): ApiResponse
+}
---
+++ src/main/kotlin/kr/co/bullets/repository/HeroRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package kr.co.bullets.repository
+
+import kr.co.bullets.models.ApiResponse
+import kr.co.bullets.models.Hero
+
+class HeroRepositoryImpl : HeroRepository {
+    override val heroes: Map<Int, List<Hero>>
+        get() = TODO("Not yet implemented")
+    override val page1: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page2: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page3: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page4: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page5: List<Hero>
+        get() = TODO("Not yet implemented")
+
+    override suspend fun getAllHeroes(page: Int): ApiResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchHeroes(name: String?): ApiResponse {
+        TODO("Not yet implemented")
+    }
+}
