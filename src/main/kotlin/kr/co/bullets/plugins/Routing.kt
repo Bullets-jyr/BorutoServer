1a2d605d63d7ca8658328b577226876f8d7c43d3
Bullets-jyr <jyr@bullets.co.kr>
2023-08-06 20:10:02 +0900

    19. Create Root Endpoint

--- src/main/kotlin/kr/co/bullets/plugins/Routing.kt
+++ src/main/kotlin/kr/co/bullets/plugins/Routing.kt
@@ -3,11 +3,13 @@ package kr.co.bullets.plugins
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import kr.co.bullets.routes.root
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+//        get("/") {
+//            call.respondText("Hello World!")
+//        }
+        root()
     }
 }
